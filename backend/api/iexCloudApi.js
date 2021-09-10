310ff7e7d8223fcfb2e9edf27e9add785d339203
Johnny Hu <johnny@dreamistlabs.com>
2021-09-10 13:46:01 -0700

    other: Comment out missing iex-cloud-api

--- backend/api/iexCloudApi.js
+++ backend/api/iexCloudApi.js
@@ -1,10 +1,12 @@
 import constants from '../const';
-import IEXCloudAPI from '@dreamistlabs/iex-cloud-api';
+// import IEXCloudAPI from '@dreamistlabs/iex-cloud-api';
 // import CheckMe from '@dreamistlabs/iexcloud-api';
 
 // CheckMe();
 
-export default new IEXCloudAPI({
-  mode: 'sandbox',
-  token: constants.IEX_TOKEN,
-});
+export default {};
+
+// export default new IEXCloudAPI({
+//   mode: 'sandbox',
+//   token: constants.IEX_TOKEN,
+// });
