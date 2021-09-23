7df9e18a901a42b633840f6fccbc74cb5e7e8fa5
Johnny Hu <johnny@dreamistlabs.com>
2021-09-22 21:34:53 -0700

    other: Improve image sizing in LightBox component

--- CLOC.md
+++ CLOC.md
@@ -1,13 +1,13 @@
-cloc|github.com/AlDanial/cloc v 1.90  T=0.17 s (1220.7 files/s, 268467.9 lines/s)
+cloc|github.com/AlDanial/cloc v 1.90  T=0.19 s (1114.4 files/s, 245170.5 lines/s)
 --- | ---
 
 Language|files|blank|comment|code
 :-------|-------:|-------:|-------:|-------:
 JavaScript|111|699|371|35139
-JSX|85|811|312|7602
+JSX|85|812|312|7614
 JSON|7|0|0|812
 Markdown|4|50|0|90
 Handlebars|1|10|5|49
 YAML|1|3|1|12
 --------|--------|--------|--------|--------
-SUM:|209|1573|689|43704
+SUM:|209|1574|689|43716
--- frontend/src/pages/Photography/Photography.jsx
+++ frontend/src/pages/Photography/Photography.jsx
@@ -2,57 +2,68 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import PhotoGallery from 'react-photo-gallery';
-import Lightbox from 'react-image-lightbox';
+import LightBox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
 
 import { GalleryImage, Helmet } from 'components';
 import { SEO } from 'const';
 import { useCopy } from 'hooks/useCopy';
+import { useEventListener } from 'hooks/useEventListener';
 import { useContentfulService } from 'services/contentfulService';
 
 import PageLayout from '../PageLayout/PageLayout';
 
-const useStyles = makeStyles(({ palette, spacing }) => ({
-  photographyLayout: {
-    padding: 0,
-  },
-  galleryContainer: {},
-  lightbox: {
-    '& .ril__inner': { backgroundColor: palette.overlay.darkest },
-    '& .ril__toolbar': { backgroundColor: palette.overlay.darkest, height: 40 },
-    '& .ril__toolbarSide': { height: 40 },
-    '& .ril__toolbarItem': { lineHeight: '40px' },
-    '& .ril__toolbarItemChild': { fontSize: '1.125rem', color: palette.grey[100] },
-    '& .ril__caption': { backgroundColor: palette.overlay.darkest, height: 40 },
-    '& .ril__captionContent': { fontSize: '0.875rem', color: palette.grey[200] },
-    '& .ril__builtinButton': { width: 'auto', height: 30, outline: 'none' },
-    '& .ril__image': { height: '100%' },
-    '& .ril__navButtons': {
-      padding: `${spacing(4)}px ${spacing(3)}px`,
-      height: 120,
-      outline: 'none',
+const useStyles = isLandscape =>
+  makeStyles(({ palette, spacing }) => ({
+    photographyLayout: {
+      padding: '0px !important',
     },
-    '& .ril-prev-button': {
-      background:
-        'rgba(0, 0, 0, 0.4) url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjIwIiBoZWlnaHQ9IjM0Ij48cGF0aCBkPSJtIDE5LDMgLTIsLTIgLTE2LDE2IDE2LDE2IDEsLTEgLTE1LC0xNSAxNSwtMTUgeiIgZmlsbD0iI0ZGRiIvPjwvc3ZnPg==") no-repeat center',
+    galleryContainer: {},
+    lightbox: {
+      '& .ril__inner': { backgroundColor: palette.overlay.darkest },
+      '& .ril__toolbar': { backgroundColor: palette.overlay.darkest, height: 40 },
+      '& .ril__toolbarSide': { height: 40 },
+      '& .ril__toolbarItem': { lineHeight: '40px' },
+      '& .ril__toolbarItemChild': { fontSize: '1.125rem', color: palette.grey[100] },
+      '& .ril__caption': { backgroundColor: palette.overlay.darkest, minHeight: 40 },
+      '& .ril__captionContent': { fontSize: '0.875rem', color: palette.grey[200] },
+      '& .ril__builtinButton': { width: 'auto', height: 30, outline: 'none' },
+      '& .ril__image': { [isLandscape ? 'height' : 'width']: '90%' },
+      '& .ril-image-current': { transform: 'none !important' },
+      '& .ril-image-next': { transform: 'translate3d(200vw, 0px, 0px) !important' },
+      '& .ril-image-prev': { transform: 'translate3d(-200vw, 0px, 0px) !important' },
+      '& .ril__navButtons': {
+        padding: `${spacing(4)}px ${spacing(3)}px`,
+        height: 120,
+        outline: 'none',
+      },
+      '& .ril-prev-button': {
+        background:
+          'rgba(0, 0, 0, 0.4) url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjIwIiBoZWlnaHQ9IjM0Ij48cGF0aCBkPSJtIDE5LDMgLTIsLTIgLTE2LDE2IDE2LDE2IDEsLTEgLTE1LC0xNSAxNSwtMTUgeiIgZmlsbD0iI0ZGRiIvPjwvc3ZnPg==") no-repeat center',
+      },
+      '& .ril-next-button': {
+        background:
+          'rgba(0, 0, 0, 0.4) url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjIwIiBoZWlnaHQ9IjM0Ij48cGF0aCBkPSJtIDEsMyAyLC0yIDE2LDE2IC0xNiwxNiAtMSwtMSAxNSwtMTUgLTE1LC0xNSB6IiBmaWxsPSIjRkZGIi8+PC9zdmc+") no-repeat center',
+      },
     },
-    '& .ril-next-button': {
-      background:
-        'rgba(0, 0, 0, 0.4) url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjIwIiBoZWlnaHQ9IjM0Ij48cGF0aCBkPSJtIDEsMyAyLC0yIDE2LDE2IC0xNiwxNiAtMSwtMSAxNSwtMTUgLTE1LC0xNSB6IiBmaWxsPSIjRkZGIi8+PC9zdmc+") no-repeat center',
-    },
-  },
-}));
+  }));
 
 export default () => {
   const { t } = useCopy();
-  const classes = useStyles();
   const content = useSelector(state => state.content);
   const { getAssets } = useContentfulService();
-  const { images } = content;
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [isLandscape, setIsLandscape] = useState(false);
+  const classes = useStyles(isLandscape)();
+  const { images } = content;
+  const nextImageIndex = (currentImageIndex + 1) % photos.length;
+  const prevImageIndex = (currentImageIndex + photos.length - 1) % photos.length;
+  const currentImage = photos[currentImageIndex];
+  const nextImage = photos[nextImageIndex];
+  const previousImage = photos[prevImageIndex];
 
   const openLightbox = useCallback((event, { photo, index }) => {
     setCurrentImageIndex(index);
@@ -64,8 +75,19 @@ export default () => {
     setViewerIsOpen(false);
   };
 
+  const updateWindowOrientation = () => {
+    if (window.innerWidth >= window.innerHeight && !isLandscape) {
+      setIsLandscape(true);
+    } else if (window.innerWidth < window.innerHeight && isLandscape) {
+      setIsLandscape(false);
+    }
+  };
+
+  useEventListener('resize', updateWindowOrientation);
+
   useEffect(() => {
     getAssets();
+    setIsLandscape(window.innerWidth >= window.innerHeight);
   }, []);
 
   useEffect(() => {
@@ -119,21 +141,18 @@ export default () => {
         onClick={openLightbox}
       />
       {viewerIsOpen ? (
-        <Lightbox
+        <LightBox
           clickOutsideToClose
           enableZoom={false}
-          imageCaption={photos[currentImageIndex].caption || ''}
-          imagePadding={1}
-          imageTitle={photos[currentImageIndex].title || undefined}
-          mainSrc={photos[currentImageIndex].src}
-          nextSrc={photos[(currentImageIndex + 1) % photos.length].src}
-          prevSrc={photos[(currentImageIndex + photos.length - 1) % photos.length].src}
+          imageCaption={currentImage.caption || ''}
+          imageTitle={currentImage.title || undefined}
+          mainSrc={currentImage.src}
+          nextSrc={nextImage.src}
+          prevSrc={previousImage.src}
           reactModalProps={{ className: classes.lightbox }}
           onCloseRequest={closeLightbox}
-          onMovePrevRequest={() =>
-            setCurrentImageIndex((currentImageIndex + photos.length - 1) % photos.length)
-          }
-          onMoveNextRequest={() => setCurrentImageIndex((currentImageIndex + 1) % photos.length)}
+          onMovePrevRequest={() => setCurrentImageIndex(prevImageIndex)}
+          onMoveNextRequest={() => setCurrentImageIndex(nextImageIndex)}
         />
       ) : null}
     </PageLayout>
